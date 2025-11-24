b724cf5ac2fcf00dc58b919a4830fb0cf6ffbe2c
Sjoblompwr <40866427+Sjoblompwr@users.noreply.github.com>
2025-11-24 20:38:46 +0100

    Linkedlist+test done

---
+++ src/se/hig/aod/lab0/LinkedList.java
@@ -0,0 +1,158 @@
+package se.hig.aod.lab0;
+
+/**
+ * 
+ * LinkedList implementation of List interface.
+ * Author: David Sjöblom
+ */
+public class LinkedList<T> implements PrintableList<T> {
+
+    private ListNode<T> root;
+    private int size = 0;
+
+    private class ListNode<T> {
+        private T data;
+        private ListNode<T> nextNode;
+
+        public ListNode(T data) {
+            this.data = data;
+            this.nextNode = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setNextNode(ListNode<T> nextNode) {
+            this.nextNode = nextNode;
+        }
+
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        root = null;
+    }
+
+    @Override
+    public int numberOfElements() {
+        return size;
+    }
+
+    @Override
+    public void insertFirst(T t) {
+        ListNode<T> newNode = new ListNode<>(t);
+        newNode.setNextNode(root);
+        root = newNode;
+        size++;
+    }
+
+    @Override
+    public void insertLast(T t) {
+        if (size == 0) {
+            insertFirst(t);
+            return;
+        }
+
+        ListNode<T> currentNode = root;
+
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.nextNode;
+        }
+
+        ListNode<T> newNode = new ListNode<>(t);
+        currentNode.nextNode = newNode;
+        size++;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            throw new ListEmptyException("Listan är tom");
+        }
+
+        T element = root.getData();
+        root = root.nextNode;
+        size--;
+
+        return element;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            throw new ListEmptyException("Listan är tom");
+        }
+
+        ListNode<T> currentNode = root;
+
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.nextNode;
+        }
+
+        T element = currentNode.getData();
+        currentNode = null;
+        size--;
+
+        return element;
+    }
+
+    @Override
+    public T getFirst() {
+        if (isEmpty()) {
+            throw new ListEmptyException("Listan är tom");
+        }
+        return root.getData();
+    }
+
+    @Override
+    public T getLast() {
+        if (isEmpty()) {
+            throw new ListEmptyException("Listan är tom");
+        }
+
+        ListNode<T> currentNode = root;
+
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.nextNode;
+        }
+        
+        return currentNode.getData();
+    }
+
+    @Override
+    public String toStringRecursive() {
+        return toStringNodeRecursive(root);
+    }
+
+    public String toStringNodeRecursive(ListNode<T> node) {
+        if (node == null)
+            return "";
+        if (node.nextNode != null)
+            return node.data.toString() + toStringNodeRecursive(node.nextNode);
+        else
+            return node.data.toString();
+    }
+
+    @Override
+    public String toStringReverseRecursive() {
+        return toStringNodeReverseRecursive(root);
+    }
+
+    public String toStringNodeReverseRecursive(ListNode<T> node) {
+        if (node == null)
+            return "";
+        if (node.nextNode != null)
+            return toStringNodeReverseRecursive(node.nextNode) + node.data.toString();
+        else
+            return node.data.toString();
+    }
+
+}
---
+++ test/se/hig/aod/lab0/LinkedListTest.java
@@ -0,0 +1,221 @@
+package se.hig.aod.lab0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LinkedListTest {
+
+	private LinkedList<Integer> listUnderTest;
+	private final int[] fixture = { 1, 2, 3, 4, 5 };
+
+	/**
+	 * Resets list to a new empty LinkedList-instance
+	 * before each test method is executed.
+	 * 
+	 * @throws Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		listUnderTest = new LinkedList<Integer>();
+	}
+
+	/**
+	 * Sets list to null after each test method is executed.
+	 * 
+	 * @throws Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+		listUnderTest = null;
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#isEmpty()}.
+	 */
+	@Test
+	public void testEmptyListIsEmpty() {
+		assertTrue(listUnderTest.isEmpty(), "An empty list instance should not contain any item");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#isEmpty()}.
+	 */
+	@Test
+	public void testInitializedListIsNotEmpty() {
+		initializeList();
+		assertFalse(listUnderTest.isEmpty(), "A list instance containing elements should not be empty");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#clear()}.
+	 */
+	@Test
+	public void testClearOnEmptyList() {
+		listUnderTest.clear();
+		assertTrue(listUnderTest.isEmpty(), "An empty list instance that is cleared should not contain any item");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#clear()}.
+	 */
+	@Test
+	public void testClearInitializedList() {
+		initializeList();
+		listUnderTest.clear();
+		assertTrue(listUnderTest.isEmpty());
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#numberOfElements()}.
+	 */
+	@Test
+	public void testNumberOfElementsOnEmptyList() {
+		assertTrue(listUnderTest.numberOfElements() == 0);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#numberOfElements()}.
+	 */
+	@Test
+	public void testNumberOfElementsOnInitializedList() {
+		initializeList();
+		assertTrue(listUnderTest.numberOfElements() == 5);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#insertFirst()}.
+	 */
+	@Test
+	public void testInsertFirstOnEmptyList() {
+		listUnderTest.insertFirst(42);
+		assertTrue(42 == listUnderTest.getFirst());
+		assertTrue(listUnderTest.numberOfElements() == 1);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#insertFirst()}.
+	 */
+	@Test
+	public void testInsertFirstOnInitializedList() {
+		initializeList();
+		listUnderTest.insertFirst(42);
+		assertTrue(listUnderTest.getFirst() == 42);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#insertLast()}.
+	 */
+	@Test
+	public void testInsertLastOnEmptyList() {
+		listUnderTest.insertLast(42);
+		assertTrue(listUnderTest.getLast() == 42);
+		assertTrue(listUnderTest.numberOfElements() == 1);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#insertLast()}.
+	 */
+	@Test
+	public void testInsertLastOnInitializedList() {
+		initializeList();
+		listUnderTest.insertLast(42);
+		assertTrue(listUnderTest.getLast() == 42);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#removeFirst()}.
+	 */
+	@Test
+	public void testRemoveFirstOnEmptyList() {
+		assertThrows(ListEmptyException.class, () -> listUnderTest.removeFirst(),
+				"removeFirst() on empty list should throw ListEmptyException");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#removeFirst()}.
+	 */
+	@Test
+	public void testRemoveFirstOnInitializedList() {
+		initializeList();
+		assertTrue(listUnderTest.removeFirst() == 1);
+		assertTrue(listUnderTest.numberOfElements()  == 4);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#removeLast()}.
+	 */
+	@Test
+	public void testRemoveLastOnEmptyList() {
+		assertThrows(ListEmptyException.class, () -> listUnderTest.removeLast(),
+				"removeLast() on empty list should throw ListEmptyException");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#removeLast()}.
+	 */
+	@Test
+	public void testRemoveLastOnInitializedList() {
+		initializeList();
+		assertTrue(listUnderTest.removeLast() == 5);
+		assertTrue(listUnderTest.numberOfElements()  == 4);
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#getFirst()}.
+	 */
+	@Test
+	public void testGetFirstOnEmptyList() {
+		assertThrows(ListEmptyException.class, () -> listUnderTest.getFirst(),
+				"GetFirst() on empty list should throw ListEmptyException");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#getFirst()}.
+	 */
+	@Test
+	public void testGetFirstOnInitializedList() {
+		initializeList();
+		assertEquals(1, listUnderTest.getFirst(), "GetFirst() should return first element in list");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#getLast()}.
+	 */
+	@Test
+	public void testGetLastOnEmptyList() {
+		assertThrows(ListEmptyException.class, () -> listUnderTest.getLast(),
+				"getLast() on empty list should throw ListEmptyException");
+	}
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#getLast()}.
+	 */
+	@Test
+	public void testGetLastOnInitializedList() {
+		initializeList();
+		assertTrue(listUnderTest.getLast() == 5);
+	}
+
+
+	/**
+	 * Test method for {@link se.hig.aod.lab0.LinkedList#toStringRecursive()}.
+	 * 
+	 */
+	@Test
+	public void testToStringRecursive(){
+		initializeList();
+		System.err.println(listUnderTest.toStringRecursive());
+		System.err.println(listUnderTest.toStringReverseRecursive());
+	}
+
+
+	private void initializeList() {
+		for (int i = 0; i < fixture.length; i++) {
+			listUnderTest.insertLast(fixture[i]);
+		}
+	}
+
+}
